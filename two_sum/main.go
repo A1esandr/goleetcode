ae9c7694edfabc2175e4b92667ac3a72b4376b8b
Alexander Kurakin <A1esandr@users.noreply.github.com>
2022-04-28 12:14:30 +0500

    Update main.go

--- two_sum/main.go
+++ two_sum/main.go
@@ -14,5 +14,16 @@ func twoSum(nums []int, target int) []int {
     return []int{}
 }
 
+func twoSumV2(nums []int, target int) []int {
+    for i, el := range nums {
+        for ind:=i+1;ind<len(nums);ind++ {
+            if result := el + nums[ind]; result == target {
+                return []int{i, ind}
+            }
+        }
+    }
+    return []int{}
+}
+
 func main() {
 }
